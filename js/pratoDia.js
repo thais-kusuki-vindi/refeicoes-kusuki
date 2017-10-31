02bddbbbfcb97b3b4b718d7fcfa69416f7f183a5
Thais Kusuki <thais.kusuki@gmail.com>
2017-10-31 18:42:13 -0200

    Inclusão do Prato do Dia em JSON na página de Cardápio

---
+++ js/pratoDia.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+	
+	//Leitura do arquivo JSON
+	$.getJSON('js/pratoDia.json', function(myData) {
+		
+
+		// Variáveis de datas para preencher o prato do dia
+		var dt = new Date();
+		var dia = dt.getDate();
+		var mes = dt.getMonth() + 1;
+		var dtcompleta = dia + '/' + mes;
+
+		for(var i = 0; i < myData.length; i++) {
+			var data = myData[i]['data'];
+			var prato = myData[i]['prato'];
+
+			// Verifica a data de hoje e adiciona o prato do dia
+			if(dtcompleta == data) {
+				$('#especial-dia').html(data + ' - ' + prato);
+				return false;
+			}
+			else {
+				$('#especial-dia').html('Consulte pelo telefone (11) 5589-6073');
+			}
+			
+
+		}
+	});
+});
